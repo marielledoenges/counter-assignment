231e0dcabe49f4da1772f98b5312e0672ec99f17
Marielle Doenges <madoenges66@gmail.com>
2024-11-14 15:04:50 -0500

    redoing main

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,44 +1,22 @@
 // import necessary react testing library helpers here
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
 // import the Counter component here
-import Counter from '../components/Counter';
+
+beforeEach(() => {
+  // Render the Counter component here
+})
 
 test('renders counter message', () => {
-  render(<Counter />);
-  const counterMsg = screen.getByText(/Counter/i);
-  expect(counterMsg).toBeInTheDocument();
+  // Complete the unit test below based on the objective in the line above
 });
 
 test('should render initial count with value of 0', () => {
-  render(<Counter />);
-  const countVal = screen.getByTestId('count');
-  expect(countVal.textContent).toBe('0');
+  // Complete the unit test below based on the objective in the line above
 });
 
-test('clicking + increments the count', async () => {
-  //const user = userEvent.setup();
-  render(<Counter/>);
-  const incrementButton = screen.getByText('+');
-  const countVal = screen.getByTestId('count');
-  
-  await userEvent.click(incrementButton);
-  expect(countVal.textContent).toBe('1');
-  
-  await userEvent.click(incrementButton);
-  expect(countVal.textContent).toBe('2');
+test('clicking + increments the count', () => {
+  // Complete the unit test below based on the objective in the line above
 });
 
-test('clicking - decrements the count', async () => {
-  render(<Counter />);
-
-  //need to be able to interact with counter button and the count var - get them
-  const decrementButton = screen.getByText('-');
-  const countVal = screen.getByTestId('count');
-  
-  await userEvent.click(decrementButton);
-  expect(countVal.textContent).toBe('-1');
-  
-  await userEvent.click(decrementButton);
-  expect(countVal.textContent).toBe('-2');
+test('clicking - decrements the count', () => {
+  // Complete the unit test below based on the objective in the line above
 });
